eea1d3e470097a3283a3b32295420f7a17589fcb
Rebecca Januario <65727310+rebeccajanuario@users.noreply.github.com>
2022-03-10 20:25:48 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace teste
+{ 
+    class Program
+    {
+        static void Main(string[]args)
+        {
+            string texto;
+            Console.WriteLine("Digite um texto:");
+            texto = Console.ReadLine();
+
+            if(string.IsNullOrEmpty(texto))
+            {
+                Console.WriteLine("Sem texto");
+            }
+            else
+            {
+                Console.WriteLine(texto);
+
+            }
+        }
+    }
+    
+}
+
+
